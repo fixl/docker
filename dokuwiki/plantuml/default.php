76e81bb5672ffce7a598f7c85ebd8b7f264c5ff5
Stefan Fuchs <stefan@aeora.io>
2016-08-28 16:51:47 +1000

    Add plantuml configuration

---
+++ dokuwiki/plantuml/default.php
@@ -0,0 +1,8 @@
+<?php
+
+$conf['render_local']='1';
+$conf['java']='/usr/bin/java';
+$conf['jar']='/dokuwiki/lib/plugins/plantuml/local/plantuml.jar';
+$conf['remote_url']='';
+$conf['button_enabled']='1';
+$conf['button_icon']='res/icon1.png';
